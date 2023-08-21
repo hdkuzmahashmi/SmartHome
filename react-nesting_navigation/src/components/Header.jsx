3359c2920e300b61c90ccc8c60b6b4bbbc2f2a40
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-21 11:54:04 +0200

    Navigation

---
+++ react-nesting_navigation/src/components/Header.jsx
@@ -0,0 +1,5 @@
+import "../styles.css";
+
+export default function Header({ children }) {
+  return <header className="header">{children}</header>;
+}
---
+++ react-nesting_navigation/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import logo from "../img/logo.jpg";
+import Image from "./Image";
+export default function Logo() {
+  return (
+    <a href="#">
+      <Image src={logo} alt="logo" />
+    </a>
+  );
+}

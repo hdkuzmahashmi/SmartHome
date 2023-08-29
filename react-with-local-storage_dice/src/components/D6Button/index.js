870c497a053c3c627b55d0e0cea5314a3dc632aa
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-29 11:39:52 +0200

    add dice app

---
+++ react-with-local-storage_dice/src/components/D6Button/index.js
@@ -0,0 +1,22 @@
+import D6 from "../D6";
+import "./D6Button.css";
+
+export function D6Button({ value, onRoll }) {
+  // const [rolls, setRolls] = useState([]);
+
+  // const handleRoll = () => {
+  //   setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  // };
+
+  //const currentRollValue = rolls[0]?.value;
+
+  return (
+    // <button className="d6-button" type="button" onClick={handleRoll}>
+    //   <D6 value={currentRollValue} />
+    // </button>
+
+    <button className="d6-button" type="button" onClick={onRoll}>
+      <D6 value={value} />
+    </button>
+  );
+}

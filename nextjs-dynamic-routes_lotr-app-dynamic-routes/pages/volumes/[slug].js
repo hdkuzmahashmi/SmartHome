ba355bf15abb7411d1bfbaeef95454b5b8f8457f
uzmahashmi <uzma.hashmi@outlook.com>
2023-09-04 11:52:14 +0200

    add dynamic routes

---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/[slug].js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import Link from "next/link";
+import { volumes } from "../../lib/data.js";
+import { useRouter } from "next/router";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+  console.log("slug", router.query);
+
+  //   const volumeIndex = volumes.findIndex(({ slug }) => slug === slugg);
+
+  //   const volume = volumes[volumeIndex];
+  //   const nextVolume = volumes[volumeIndex + 1];
+  //   const previousVolume = volumes[volumeIndex - 1];
+  //   console.log("nextvolume", nextVolume);
+  //   console.log("previousVolume", previousVolume);
+
+  const currentVolume = volumes.find((volume) => volume.slug === slug);
+
+  if (!currentVolume) {
+    return null;
+  }
+
+  const { title, description, cover, books } = currentVolume;
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handelRandomVolume() {
+    const randomVolume = getRandomElement(volumes);
+
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
+  return (
+    <>
+      <Link href="/volumes">← All Volumes</Link>
+
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <ul>
+        {books.map(({ ordinal, title }) => (
+          <li key={title}>
+            {ordinal}: <strong>{title}</strong>
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={cover}
+        alt={`Cover image of ${title}`}
+        width={140}
+        height={230}
+      />
+      {/* {previousVolume ? (
+        <div>
+          <Link href={`/volumes/${previousVolume.slug}`}>
+            ← Previous Volume: {previousVolume.title}
+          </Link>
+        </div>
+      ) : null}
+      {nextVolume ? (
+        <div>
+          <Link href={`/volumes/${nextVolume.slug}`}>
+            Next Volume: {nextVolume.title} →
+          </Link>
+        </div>
+      ) : null}
+*/}
+      {
+        <button type="button" onClick={handelRandomVolume}>
+          {" "}
+          Random Volume
+        </button>
+      }
+    </>
+  );
+}
---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+import Router from "next/router";
+import { uid } from "uid";
+
+export default function Volumes() {
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handelRandomVolume() {
+    const randomVolume = getRandomElement(volumes);
+
+    Router.push(`/volumes/${randomVolume.slug}`);
+  }
+
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map((volume) => {
+          return (
+            <li key={uid()}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
+
+        {/* <li>
+          <Link href="/volumes/the-fellowship-of-the-ring">
+            The Fellowship of the Ring
+          </Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-two-towers">The Two Towers</Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-return-of-the-king">
+            The Return of the King
+          </Link>
+        </li> */}
+      </ul>
+      <button type="button" onClick={handelRandomVolume}>
+        {" "}
+        Random Volume
+      </button>
+    </>
+  );
+}

e918534e8d79432a238ae4af812e1507d731f864
uzmahashmi <uzma.hashmi@outlook.com>
2023-09-11 12:40:37 +0200

    refactor code

--- react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,27 +1,29 @@
 import Controls from "../Controls/index";
 import Map from "../Map/index";
-import swr from "swr";
+import useSwr from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function ISSTracker() {
-  const { data, error, isLoading, mutate } = swr(URL, fetcher, {
-    interval: 5000,
+  const { data, error, isLoading, mutate } = useSwr(URL, fetcher, {
+    refreshInterval: 5000,
   });
 
   const coords = data
     ? { longitude: data.longitude, latitude: data.latitude }
     : { longitude: 0, latitude: 0 };
 
+  const { longitude, latitude } = coords;
+
   if (isLoading) return <h1>Loading...</h1>;
   if (error) return <h1>Error: {error.message}</h1>;
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={longitude} latitude={latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
+        longitude={longitude}
+        latitude={latitude}
         onRefresh={() => mutate()}
       />
     </main>
